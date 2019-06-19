20026a8ed77515104e4fed136a4f093c9731a557
Matias Carrasco Kind <mgckind@gmail.com>
2019-06-19 16:21:51 -0500

    re-factorization

--- js9_web.py
+++ js9_web.py
@@ -1,28 +1,33 @@
 import tornado.ioloop
 import tornado.web
 import os
+import subprocess
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html",jid=None)
+        self.render("index.html", jid=None)
 
 class NewHandler(tornado.web.RequestHandler):
     def get(self, jid_id):
         self.render("index.html", jid=jid_id)
 
 settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+    #"static_path": os.path.join(os.path.dirname(__file__), "static"),
+    "static_path": '.',
 }
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
-        (r"/([0-9a-zA-Z]+)", NewHandler),
-        (r"/(.*)", tornado.web.StaticFileHandler, {"path": os.path.dirname(__file__), "default_filename": "js9Prefs.json"}),
-        (r"/(.*)", tornado.web.StaticFileHandler, {"path": os.path.dirname(__file__), "default_filename": "favicon.ico"})
+        (r"/jjs9/", MainHandler),
+        (r"/jjs9/([0-9a-zA-Z]+)", NewHandler),
+        (r"/jjs9/(.*)", tornado.web.StaticFileHandler, {"path": os.path.dirname(__file__), "default_filename": "js9Prefs.json"}),
+        (r"/jjs9/(.*)", tornado.web.StaticFileHandler, {"path": os.path.dirname(__file__), "default_filename": "favicon.ico"})
     ], **settings)
 
 if __name__ == '__main__':
+    subprocess.Popen(['node', 'js9Helper.js'])
+    port = 8000
     app = make_app()
-    app.listen(8000)
+    app.listen(port)
+    print('Listening at port {}'.format(port))
     tornado.ioloop.IOLoop.current().start()
